9109afd50a3b6488e380ab3e86e2c7ff51c980a5
MR ARFAN <46324706+muhdarfan@users.noreply.github.com>
2022-01-25 18:37:15 +0800

    Update docker

--- api.py
+++
@@ -1,8 +0,0 @@
-from app import app
-from OpenSSL import SSL
-context = SSL.Context(SSL.PROTOCOL_TLSv1_2)
-context.use_privatekey_file('server.key')
-context.use_certificate_file('server.crt')   
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000, ssl_context=context)
